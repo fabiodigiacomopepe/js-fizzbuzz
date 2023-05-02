04ed3fc214abaa98d36e49302c7483703aca2896
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-05-02 17:04:02 +0200

    Add css

--- js/main.js
+++ js/main.js
@@ -12,10 +12,13 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {   // se i è multiplo sia di 3 che di 5    appendo in span FIZZBUZZ
         quadrato.append("fizzbuzz");
+        quadrato.classList.add("red");
     } else if (i % 3 === 0) {           // se i è multiplo solo di 3            appendo in span FIZZ
         quadrato.append("fizz");
+        quadrato.classList.add("green");
     } else if (i % 5 === 0) {           // se i è multiplo solo di 5            appendo in span     BUZZ
         quadrato.append("buzz");
+        quadrato.classList.add("yellow");
     } else                              // se i non è ne multiplo ne di 3 e/o 5 appendo in span numero
         quadrato.append(i);
     
